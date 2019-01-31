a3aff4378aa48c724b29604d7f13a095ab00f9d7
krisztina <nkrisztina82@gmail.com>
2019-01-31 21:06:51 +0100

    feat(models/Address): create model Address

--- PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/models/Address.java
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/models/Address.java
@@ -1,4 +1,51 @@
 package com.krisztinanmth.phonebook.models;
 
 public class Address {
+
+  private String country;
+  private String zipCode;
+  private String city;
+  private String street;
+
+  public Address() {
+  }
+
+  public Address(String country, String zipCode, String city, String street) {
+    this.country = country;
+    this.zipCode = zipCode;
+    this.city = city;
+    this.street = street;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
 }
