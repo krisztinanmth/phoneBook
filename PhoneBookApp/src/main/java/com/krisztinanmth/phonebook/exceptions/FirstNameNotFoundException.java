203726444c5e8a7009eedddd00030b5dcdb77972
krisztina <nkrisztina82@gmail.com>
2019-02-01 14:03:12 +0100

    feat(exceptions): create FirstNameNotFoundException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/FirstNameNotFoundException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class FirstNameNotFoundException extends Exception {
+
+  public FirstNameNotFoundException(String message) {
+    super(message);
+  }
+}
