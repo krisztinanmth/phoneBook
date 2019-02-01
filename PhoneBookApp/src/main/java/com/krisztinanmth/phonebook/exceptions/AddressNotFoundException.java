3573d3b2d2ef0d959af63f0368ade224567bf603
krisztina <nkrisztina82@gmail.com>
2019-02-01 14:09:45 +0100

    feat(exceptions): create AddressNotFoundException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/AddressNotFoundException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class AddressNotFoundException extends Exception {
+
+  public AddressNotFoundException(String message) {
+    super(message);
+  }
+}
