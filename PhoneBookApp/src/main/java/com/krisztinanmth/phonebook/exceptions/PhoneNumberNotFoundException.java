112eb2ce1ea3a7dd970df21175288ff8ddadc015
krisztina <nkrisztina82@gmail.com>
2019-02-02 15:51:58 +0100

    feat(exceptions): create PhoneNumberNotFoundException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/PhoneNumberNotFoundException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class PhoneNumberNotFoundException extends Exception {
+
+  public PhoneNumberNotFoundException(String message) {
+    super(message);
+  }
+}
