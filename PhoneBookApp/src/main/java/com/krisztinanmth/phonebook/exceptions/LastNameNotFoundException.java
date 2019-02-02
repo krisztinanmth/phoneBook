3ce188a643b1d289ebdd2b0f568bc4b7237d901a
krisztina <nkrisztina82@gmail.com>
2019-02-02 10:55:33 +0100

    feat(exceptions): create LastNameNotFoundException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/LastNameNotFoundException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class LastNameNotFoundException extends Exception {
+
+  public LastNameNotFoundException(String message) {
+    super(message);
+  }
+}
