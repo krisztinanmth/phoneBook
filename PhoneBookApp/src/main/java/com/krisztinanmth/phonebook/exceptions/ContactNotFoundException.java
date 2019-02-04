d5146446733027bdfe3cdba184795b058fe0be92
krisztina <nkrisztina82@gmail.com>
2019-02-04 12:46:58 +0100

    feat(exceptions): create ContactNotFoundException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/ContactNotFoundException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class ContactNotFoundException extends Exception {
+
+  public ContactNotFoundException(String message) {
+    super(message);
+  }
+}
