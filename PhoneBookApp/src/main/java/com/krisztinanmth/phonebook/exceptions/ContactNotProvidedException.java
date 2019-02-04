14c0716fddeedf649dedfeea26501a0e2a614523
krisztina <nkrisztina82@gmail.com>
2019-02-04 14:22:51 +0100

    feat(exceptions): create ContactNotProvidedException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/ContactNotProvidedException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class ContactNotProvidedException extends Exception {
+
+  public ContactNotProvidedException(String message) {
+    super(message);
+  }
+}
