efea3aa56ff74c039bd8421c1f299e4257d6cf23
krisztina <nkrisztina82@gmail.com>
2019-02-04 11:23:00 +0100

    feat(exceptions): create BirthdayNotFoundException

---
+++ PhoneBookApp/src/main/java/com/krisztinanmth/phonebook/exceptions/BirthdayNotFoundException.java
@@ -0,0 +1,8 @@
+package com.krisztinanmth.phonebook.exceptions;
+
+public class BirthdayNotFoundException extends Exception {
+
+  public BirthdayNotFoundException(String message) {
+    super(message);
+  }
+}
