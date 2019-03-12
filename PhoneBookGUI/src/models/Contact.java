436dc4619301985c02096e207f14f998efc6791a
Nemeth Krisztina <knemeth@b2international.com>
2019-03-12 14:04:12 +0100

    chore(models): create Contact and Address class

---
+++ PhoneBookGUI/src/models/Contact.java
@@ -0,0 +1,72 @@
+package models;
+
+import java.util.List;
+
+public class Contact {
+
+  private String firstName;
+  private String lastName;
+  private String dateOfBirth;
+  private List<String> phoneNumber;
+  private List<Address> address;
+
+  public Contact() {
+  }
+
+  public Contact(String firstName, String lastName, String dateOfBirth, List<String> phoneNumber, List<Address> address) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.phoneNumber = phoneNumber;
+    this.address = address;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getName() {
+    return firstName + " " + lastName;
+  }
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public List<String> getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(List<String> phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public List<Address> getAddress() {
+    return address;
+  }
+
+  public void setAddress(List<Address> address) {
+    this.address = address;
+  }
+
+  @Override
+  public String toString() {
+    return firstName + " " + lastName + " " + dateOfBirth + " " + phoneNumber + " " + address.toString();
+  }
+}
