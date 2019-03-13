ef4350ef29acfc27fcdedb197ae925167f40ac5a
Nemeth Krisztina <knemeth@b2international.com>
2019-03-13 16:49:28 +0100

    chore(GUI_swt): will be deleted later on

--- GUI_swt/GUI_swt/src/main/java/Client.java
+++ GUI_swt/GUI_swt/src/main/java/Client.java
@@ -1,13 +1,28 @@
+import java.util.List;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+import com.krisztinanmth.phonebook.models.Contact;
+import com.krisztinanmth.phonebook.services.JSONService;
+import com.krisztinanmth.phonebook.services.JSONServiceImpl;
 
 public class Client {
 	
+	private List<Contact> contacts;
+	private static JSONService jsonService = new JSONServiceImpl();
+	
+	private static final String JSON_PATH = "~/Desktop/phoneBookApp/phoneBook/PhoneBookApp/src/main/resources/contacts.json";
+	
 	public Client() {
+		//jsonService = new JSONServiceImpl();
+		contacts = jsonService.readFromJSON(JSON_PATH);
 		initUI();
 	}
 
@@ -23,7 +38,15 @@ public class Client {
 		mainComp.setLayout(new GridLayout(2, false));
 		mainComp.setLayoutData(mainData);
 		
-		createHeaderCom(mainComp);
+//		createHeaderCom(mainComp);
+		if (contacts != null) {
+			final Tree tree = new Tree(mainComp, SWT.V_SCROLL);
+			for (int i = 0; i < contacts.size(); i++) {
+				TreeItem item = new TreeItem(tree, SWT.NONE);
+				item.setText(String.valueOf(contacts.get(i)));
+			}
+		}
+		
 		
 		shell.open();
 		
@@ -37,10 +60,10 @@ public class Client {
 		
 	}
 
-	private void createHeaderCom(Composite mainComp) {
-		// TODO Auto-generated method stub
-		
-	}
+//	private void createHeaderCom(Composite mainComp) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 
 	public static void main(String[] args) {
 		
