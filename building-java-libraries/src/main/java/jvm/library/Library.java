9bf7ece78b4a478eda0182b033fc9f497c4324fc
Nemeth Krisztina <knemeth@b2international.com>
2019-03-13 15:08:48 +0100

    chore(build-java-libraries): create jar from gradle project .... gradle tutorial

---
+++ building-java-libraries/src/main/java/jvm/library/Library.java
@@ -0,0 +1,10 @@
+/*
+ * this is a library class, with one method, that returns true
+ */
+package jvm.library;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
