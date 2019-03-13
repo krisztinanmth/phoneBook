1d01676c898e7e01beb59ad449cb3fc28fdcd114
Nemeth Krisztina <knemeth@b2international.com>
2019-03-13 13:13:13 +0100

    feat(Client/GUI_swt): create Client class... now with working shell

--- GUI_swt/GUI_swt/src/main/java/Client.java
+++ GUI_swt/GUI_swt/src/main/java/Client.java
@@ -1,19 +1,51 @@
-
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class Client {
 	
 	public Client() {
-		initUi();
+		initUI();
 	}
 
-	private void initUi() {
-		//final Display display = new Display();
+	private void initUI() {
+		final Display display = new Display();
+		final Shell shell = new Shell(display);
+		
+		shell.setLayout(new GridLayout(1, true));
+		shell.setText("phonebook");
+		
+		final Composite mainComp = new Composite(shell, SWT.NONE);
+		final GridData mainData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		mainComp.setLayout(new GridLayout(2, false));
+		mainComp.setLayoutData(mainData);
+		
+		createHeaderCom(mainComp);
+		
+		shell.open();
+		
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		
+		display.dispose();
+		
+	}
+
+	private void createHeaderCom(Composite mainComp) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		@SuppressWarnings("unused")
+		Client client = new Client();
 	}
 
 }
